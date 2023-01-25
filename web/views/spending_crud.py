d889d5f839084bc42f9ff643f303f6115c4f4a59
lemmehoop <lemmehoop@gmail.com>
2023-01-25 19:29:20 +0300

    + chart

--- web/views/spending_crud.py
+++ web/views/spending_crud.py
@@ -1,6 +1,10 @@
+from datetime import date, timedelta
+
+from django.db.models import Count
 from django.http import JsonResponse
 from django.views.generic import ListView
 
+from web.enums import Category
 from web.forms import SpendingForm
 from web.models import Spending
 
@@ -13,29 +17,40 @@ class SpendingsListView(ListView):
             return Spending.objects.filter(user=self.request.user)
         return Spending.objects.none()
 
+    def get_values(self):
+        res = []
+        aggregated = Spending.objects.filter(user=self.request.user)\
+            .filter(created_at__gt=date.today()-timedelta(days=31))\
+            .values("category")\
+            .annotate(count=Count("id")).order_by("-count")
+        for spending in aggregated:
+            res.append([Category[spending["category"]].label, spending["count"]])
+
+        return res
+
     def get_context_data(self, *, object_list=None, **kwargs):
         if not self.request.user.is_authenticated:
             return {}
         return {
             **super(SpendingsListView, self).get_context_data(),
             "form": SpendingForm,
+            "values": self.get_values()
         }
 
 
 def add_spending(request):
     title = request.POST.get("title")
     amount = request.POST.get("amount")
-    category_id = request.POST.get("category")
+    category = request.POST.get("category")
     spending = Spending.objects.create(
         title=title,
         amount=amount,
-        category_id=category_id,
+        category=category,
         user=request.user
     )
 
     return JsonResponse({
         "title": spending.title,
         "amount": spending.amount,
-        "category": spending.category.title,
-        "csrftoken": request.POST.get("csrfmiddlewaretoken")
+        "category": spending.category,
     })
