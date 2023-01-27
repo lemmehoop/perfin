c502f1fbca3f17ff1407531012bef883a01fe53b
lemmehoop <lemmehoop@gmail.com>
2023-01-27 16:46:17 +0300

    ! styles in stats page

--- web/forms.py
+++ web/forms.py
@@ -105,7 +105,10 @@ class UserForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.label_suffix = ""
         for attr, value in self.fields.items():
-            self.fields[attr].widget.attrs.update({"class": "form-control mb-3"})
+            if attr == "name":
+                self.fields[attr].widget.attrs.update({"class": "form-control mb-3"})
+            else:
+                self.fields[attr].widget.attrs.update({"class": "form-control mb-2"})
 
     class Meta:
         model = User
