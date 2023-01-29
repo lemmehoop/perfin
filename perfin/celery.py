c1a0542b9606e0fabdbd0397185f5f662f86c1a6
lemmehoop <lemmehoop@gmail.com>
2023-01-29 13:21:19 +0300

    + celery

---
+++ perfin/celery.py
@@ -0,0 +1,9 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "perfin.settings")
+
+app = Celery("notes")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.autodiscover_tasks()
