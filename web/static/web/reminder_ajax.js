32751f29a12747f5942569e85b1814ff1e6a67b1
lemmehoop <lemmehoop@gmail.com>
2023-01-30 14:35:52 +0300

    + reminder adding ajax

---
+++ web/static/web/reminder_ajax.js
@@ -0,0 +1,34 @@
+$('#add').on("click", function () {
+    $.ajax({
+        url: "add_reminder",
+        method: 'post',
+        dataType: 'json',
+        data: $('#r_form').serialize(),
+        success: function (response) {
+            let new_div = document.createElement("div")
+            new_div.className = "card mb-2 mt-1"
+            new_div.innerHTML = "<div class=\"row\">" +
+                "                            <div class=\"col\">" +
+                "                                <div class=\"row\">" +
+                "                                    <p class=\"card-title mt-1 mb-1 fw-normal flex-column\">" + response.title + "</p>" +
+                "                                </div>" +
+                "                                <div class=\"row\">" +
+                "                                    <p class=\"m-0 time\">Только что</p>" +
+                "                                </div>" +
+                "                            </div>" +
+                "                            <div class=\"col\">" +
+                "                                <div class=\"row\">" +
+                "                                    <p class=\"m-0 mx-auto text-danger\">-"+ response.amount +" ₽</p>" +
+                "                                </div>" +
+                "                                <div class=\"row\">" +
+                "                                    <p class=\"m-0 mx-auto\">" + response.text.substr(0, 15) + "...</p>" +
+                "                                </div>" +
+                "                            </div>" +
+                "                        </div>"
+            parent_div = document.getElementById("list");
+            first_child = parent_div.firstChild;
+            parent_div.insertBefore(new_div, first_child);
+            document.getElementById("r_form").reset();
+        },
+    })
+})
