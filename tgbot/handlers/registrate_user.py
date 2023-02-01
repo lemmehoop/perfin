f7bc9620d5f5f75024fa7b8847fa828566163efc
lemmehoop <lemmehoop@gmail.com>
2023-02-01 19:07:33 +0300

    + checking password and ending conversation

--- tgbot/handlers/registrate_user.py
+++ tgbot/handlers/registrate_user.py
@@ -1,45 +1,41 @@
 import os
 import django
 from asgiref.sync import sync_to_async
+from django.contrib.auth.hashers import check_password
 
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, ConversationHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'perfin.settings')
 django.setup()
 
 from web.models import User
 
-REGISTRATION, WAIT = range(2)
+REGISTRATION, = range(1)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
 
     await update.message.reply_text(
-        f"Привет {user.name}!\nВведи почту, на которую зарегистрирован аккаунт в perfin",
+        f"Привет {user.name}!\nВведите почту и пароль аккаунта perfin через пробел",
     )
 
     return REGISTRATION
 
 
 async def registrate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    email = update.message.text
+    email, password = update.message.text.split()
     try:
         user = await sync_to_async(User.objects.get)(email=email)
     except User.DoesNotExist:
         user = None
 
-    if user:
+    if user and check_password(password, user.password):
         await update.message.reply_text(
             f"Аккаунт с почтой {user.email} привязан к этому чату\nОжидаю уведомления...",
         )
+        return ConversationHandler.END
     else:
-        await update.message.reply_text("Пользователя с такой почтой не существует\nПопробуйте еще раз")
+        await update.message.reply_text("Пароль или почта неправильные\nПопробуйте еще раз")
         return REGISTRATION
-
-
-# async def wait_messages(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     await update.message.reply_text(
-#         f"Все завершено! Ожидаю уведомления...",
-#     )
