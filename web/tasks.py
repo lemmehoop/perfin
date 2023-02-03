204a0ba237dcec2bb02aa91dac61136fda5469ea
lemmehoop <lemmehoop@gmail.com>
2023-02-03 18:14:01 +0300

    + sending notification via telegram

--- web/tasks.py
+++ web/tasks.py
@@ -1,11 +1,11 @@
 from celery import shared_task
 
+from tgbot.functions.messages import send_message
 from web.models import Reminder
 
 
 @shared_task(name="send_notification")
 def send_notification(id: int):
     reminder = Reminder.objects.get(pk=id)
-    print("------------------------------------------")
-    print(reminder.title, reminder.text)
-    print("------------------------------------------")
+    send_message(chat_id=reminder.user.chat_id, text=reminder.title + "\n" + reminder.text)
+    print("Notification was sent.")
